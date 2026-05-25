25e5d8f4f652c73d43555c44d2161693b8cfe745
Eric Garcia <ericrivas192@gmail.com>
2026-05-25 13:18:55 -0600

    config iniciar de capas

---
+++ backend/src/main/java/com/sistemasope/aeromaya/AeromayaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasope.aeromaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeromayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeromayaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sistemasope/aeromaya/AeromayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasope.aeromaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeromayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
